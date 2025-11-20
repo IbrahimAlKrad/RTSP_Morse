2a12c41b4c6b2666ea84a9e8ab57d7d976a74d85
Jannes Bikker <jannesbikker@gmail.com>
2025-11-20 17:25:57 +0100

    feat: add frontend

---
+++ TextToSound/frontend/app/routes.ts
@@ -0,0 +1,6 @@
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+
+export default [
+    index("routes/home.tsx"),
+    route("stream", "routes/stream.ts"),
+] satisfies RouteConfig;
