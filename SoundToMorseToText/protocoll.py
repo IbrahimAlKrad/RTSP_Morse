b5b2e82ac802279b4d882f9358a1121b3e5cb4b1
Thilo <thilo.blumenberg@live.de>
2025-12-11 17:32:58 +0100

    added my files

---
+++ SoundToMorseToText/protocoll.py
@@ -0,0 +1,222 @@
+import faust
+import faust.tables
+import numpy as np
+import time
+import pickle
+import os
+
+class Message (faust.Record):
+    user : str
+    message : str
+
+app = faust.App('morsecode_protocoll',broker = 'kafka://localhost', web_port=6067)
+sr_morse = app.topic('sr_morse', value_type = Message, key_type = str)
+
+morse_alphabet = {".-":"a",
+                 "-...":"b",
+                 "-.-.":"c",
+                 "-..":"d",
+                 ".":"e",
+                 "..-.":"f",
+                 "--.":"g",
+                 "....":"h",
+                 "..":"i",
+                 ".---":"j",
+                 "-.-":"k",
+                 ".-..":"l",
+                 "--":"m",
+                 "-.":"n",
+                 "---":"o",
+                 ".--.":"p",
+                 "--.-":"q",
+                 ".-.":"r",
+                 "...":"s",
+                 "-":"t",
+                 "..-":"u",
+                 "...-":"v",
+                 ".--":"w",
+                 "-..-":"x",
+                 "-.--":"y",
+                 "--..":"z"}
+
+class MorseTreeNode:
+   
+    def __init__(self, character):
+        self.data = character
+        self.children = []
+
+    def add_child(self, Node):
+        self.children.append(Node)
+
+initializer = MorseTreeNode("initialize")
+
+# Definition of the morse tree
+#### first generation ####
+# childs of initializer
+child_e = MorseTreeNode("e") #dot
+child_t = MorseTreeNode("t") #minussign
+
+#### second generation ####
+# childs of child_e
+child_i = MorseTreeNode("i") #dot dot
+child_a = MorseTreeNode("a") #dot minussign
+
+# childs of child_t
+child_n = MorseTreeNode("n") #minussign dot
+child_m = MorseTreeNode("m") #minussign minussign
+
+#### third generation ####
+# childs of child_i
+child_s = MorseTreeNode("s") #dot dot dot
+child_u = MorseTreeNode("u") #dot dot minussign
+
+# childs of child_a
+child_r = MorseTreeNode("r") #dot minussign dot
+child_w = MorseTreeNode("w") #dot minussign minussign
+
+# childs of child_n
+child_d = MorseTreeNode("d") #minussign dot dot
+child_k = MorseTreeNode("k") #minussign dot minussign
+
+# childs of child_m
+child_g = MorseTreeNode("g") #minussign minussign dot
+child_o = MorseTreeNode("o") #minussign minussign minussign
+
+#### fourth generation ####
+# childs of child_s
+child_h = MorseTreeNode("h") #dot dot dot dot
+child_v = MorseTreeNode("v") #dot dot dot minussign
+
+# childs of child_u
+child_f = MorseTreeNode("f") #dot dot minussign dot
+
+# childs of child_r
+child_l = MorseTreeNode("l") #dot minussign dot dot
+
+# childs of child_w
+child_p = MorseTreeNode("p") #dot minussign minussign dot
+child_j = MorseTreeNode("j") #dot minussign minussign minussign
+
+# childs of child_d
+child_b = MorseTreeNode("b") #minussign dot dot dot
+child_x = MorseTreeNode("x") #minussign dot dot minussign
+
+# childs of child_k
+child_c = MorseTreeNode("c") #minussign dot minussign dot
+child_y = MorseTreeNode("y") #minussign dot minussign minussign
+
+# childs of child_g
+child_z = MorseTreeNode("z") #minussign minussign dot dot
+child_q = MorseTreeNode("q") #minussign minussign dot minussign
+            ##################################################################################################################
+
+#### first generation ####
+# childs of initializer
+initializer.add_child(child_e)
+initializer.add_child(child_t)
+
+#### second generation ####
+# Connacting the tree
+# connecting e's children to e
+child_e.add_child(child_i)
+child_e.add_child(child_a)
+
+# connecting t's children to t
+child_t.add_child(child_n)
+child_t.add_child(child_m)
+
+#### third generation ####
+# child_i's children
+child_i.add_child(child_s)
+child_i.add_child(child_u)
+
+# childs of child_a
+child_a.add_child(child_r)
+child_a.add_child(child_w)
+
+# childs of child_n
+child_n.add_child(child_d)
+child_n.add_child(child_k)
+
+# childs of child_m
+child_m.add_child (child_g)
+child_m.add_child (child_o)
+
+#### fourth generation ####
+# childs of child_s
+child_s.add_child(child_h)
+child_s.add_child(child_v)
+
+# childs of child_u
+child_u.add_child(child_f)
+
+# childs of child_r
+child_r.add_child(child_l)
+
+# childs of child_w
+child_w.add_child(child_p)
+child_w.add_child(child_j)
+
+# childs of child_d
+child_d.add_child(child_b)
+child_d.add_child(child_x)
+
+# childs of child_k
+child_k.add_child(child_c)
+child_k.add_child(child_y)
+
+# childs of child_g
+child_g.add_child(child_z)
+child_g.add_child(child_q)
+            ##################################################################################################################
+
+def translate_morse_to_text(morse_word):
+    letter = morse_word.split()
+    word=""
+    for l in range(len(letter)):
+        word=word+morse_alphabet[letter[l]]
+    return word
+
+def tree_translate_morse_to_text(morse_sequence):
+    symbol = morse_sequence.split()
+    word=""
+    for l in symbol:
+        characters=list(l)
+        letter_class=initializer
+        for i in range(len(characters)):
+            if characters[i]==".":
+                letter_class=letter_class.children[0]
+            elif characters[i]=="-":
+                letter_class=letter_class.children[1]
+
+            if i == len(characters)-1:
+                word=word+letter_class.data
+    return word
+
+
+@app.agent(sr_morse)
+async def monitor(stream):
+    async for msg in stream:
+
+        print(msg.user + ": ")
+        print(msg.message)
+        word_list = msg.message.split("   ")
+
+        sentence_translated=""
+        i=0
+        for word in word_list:
+            if i>0:
+                sentence_translated=sentence_translated+" "+translate_morse_to_text(word)
+            else:
+                sentence_translated=sentence_translated+translate_morse_to_text(word)
+            i+=1
+        print(sentence_translated)
+       
+
+if __name__ == '__main__':
+    app.main ()
+
+
+
+
+
---
+++ SoundToMorseToText/sr_sender.py
@@ -0,0 +1,104 @@
+import faust
+import asyncio
+import faust.tables
+import numpy as np
+import speech_recognition as sr
+import time
+import pickle
+import os
+
+r=sr.Recognizer()
+mic=sr.Microphone()
+
+morse_alphabet_inverse = {"a":".-",
+                 "b":"-...",
+                 "c":"-.-.",
+                 "d":"-..",
+                 "e":".",
+                 "f":"..-.",
+                 "g":"--.",
+                 "h":"....",
+                 "i":"..",
+                 "j":".---",
+                 "k":"-.-",
+                 "l":".-..",
+                 "m":"--",
+                 "n":"-.",
+                 "o":"---",
+                 "p":".--.",
+                 "q":"--.-",
+                 "r":".-.",
+                 "s":"...",
+                 "t":"-",
+                 "u":"..-",
+                 "v":"...-",
+                 "w":".--",
+                 "x":"-..-",
+                 "y":"-.--",
+                 "z":"--.."}
+
+
+def translate_single_word_to_morse(word):
+    letter = list(word)
+    symbols=""
+    for l in range(len(letter)):
+        symbols=symbols+" "+morse_alphabet_inverse[letter[l]]
+    return symbols
+
+class Message (faust.Record):
+    user : str
+    message : str
+
+def speech_to_str():
+    with mic as source:
+            audio = r.listen(source)
+
+    try:
+        return r.recognize_google(audio, language = r"en-EN") #de-DE
+    except sr.UnknownValueError:
+        print("Keine Sprache erkannt. Bitte erneut sprechen.")
+        return ""  # leere Nachricht zurückgeben
+    except sr.RequestError as e:
+        print(f"Fehler bei der Verbindung zu Google Speech API: {e}")
+        return ""
+
+app = faust.App('morsecode_sender',broker = 'kafka://localhost')
+sr_morse = app.topic('sr_morse', value_type = Message, key_type = str)
+
+async def async_input():
+    return await asyncio.to_thread(speech_to_str)
+
+
+@app.task()
+async def example_sender():
+    print("Select your username: ")
+    username = input()
+    print("You hereby entered the chatroom.")
+    while True:
+  
+        message = await async_input()
+        print(message)
+        word_list = message.lower().split()
+
+        sentence_translated=""
+        i=0
+        for word in word_list:
+            if i>0:
+                sentence_translated=sentence_translated+"   "+translate_single_word_to_morse(word)
+            else:
+                sentence_translated=sentence_translated+translate_single_word_to_morse(word)
+            i+=1
+
+        await sr_morse.send(
+            value = Message(user = username, message = sentence_translated), key = "reading" 
+        )
+
+
+       
+
+if __name__ == '__main__':
+    app.main ()
+
+
+
+
